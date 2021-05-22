6901707d6a17ea44359a0a0158a0b87373633a03
Zenpanic <sebastien.belmo@protonmail.com>
2021-05-22 11:41:36 +0200

    202105221141

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const axios = require('axios');
+
 const pingApp = () => {
-    console.log('hello !');
+    axios.get('https://artiste-promomusique.vercel.app/api/analytics/youtubeOrders')
+    .then((response) => {
+        console.log(response);
+        console.log('oki doki');
+    });
     return true;
 };
 
