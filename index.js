e8713c6d82304b23039665c35161f8cf2545113c
Zenpanic <sebastien.belmo@protonmail.com>
2021-05-23 11:30:57 +0200

    20210523130

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 axios({
-    method: 'GET',
+    method: 'get',
     url: 'https://artiste-promomusique.vercel.app/api/analytics/youtubeOrders',
     withCredentials: true,
     headers: { "Content-type": "application/json;charset=UTF-8" }
