5f917e31497f1b5482c2e8cbb4487da685405914
Zenpanic <sebastien.belmo@protonmail.com>
2021-05-23 12:15:51 +0200

    202105231215

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const axios = require('axios').default;
 
 const pingApp = async () => {
-    await axios({
+    axios({
         method: 'get',
         url: 'https://artiste-promomusique.vercel.app/api/analytics/youtubeOrders',
         withCredentials: true
     }).then((response) => {
         console.log(response);
-        console.log('oki doki!!');
+        console.log('oki doki!!!');
     }).catch((error) => {
         console.log('error!!', error)
     });
